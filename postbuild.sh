2a191f46396e6ce4b56fc9aaf63d5fc04a411d5f
Paul Duguet <opauldo@gmail.com>
2017-09-04 12:28:40 +0200

    build: Added missing build steps

--- postbuild.sh
+++ postbuild.sh
@@ -2,7 +2,6 @@
 
 copyfiles www\
           config/*\
-          Procfile\
           package.json\
           dist #Destination dir
 
@@ -12,4 +11,8 @@ copyfiles -u 1 'assets/**/*' dist/public
 if [ $# == 1 ] && [ "$1" == "--dev" ]; then
   copyfiles elm-package.debug.json webpack.config.js 'elm-stuff/**/*' dist
   mv dist/elm-package.debug.json dist/elm-package.json
+else
+  copyfiles -u 1 'client/elm/**/*' dist
+  copyfiles elm-package.release.json Procfile dist
+  mv dist/elm-package.release.json dist/elm-package.json
 fi
